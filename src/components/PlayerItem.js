2c689c887713432c8344f99018e52089bca34caf
Marvin Vrdoljak <marvin.vrdoljak@dudagroup.com>
2020-03-30 20:37:36 +0200

    favicon

--- src/components/PlayerItem.js
+++ src/components/PlayerItem.js
@@ -13,7 +13,7 @@ function PlayerItem(state) {
     <div className={"player"}>
       <h3 className="player__name">{name} { loser ? <img className="player__loser" src={loserIcon} alt="1. Hälfte verloren"/> :''} </h3>
       <div className="player__meta">
-          <span><strong>Runde: {round} &nbsp;//&nbsp;</strong></span>
+          <span><strong>Gewürfelt: {round}x&nbsp;//&nbsp;</strong></span>
           <span><strong>Batches: {batch}</strong></span>
           <span className={"player__meta__button" + (batches === 0 ? ' disable' : '')} onClick={state.addBatch.bind(this, id)}><img src={plusIcon} alt="Aufdecken"/></span>
           <span className={"player__meta__button" + (batch === 0 ? ' disable' : '')} onClick={state.removeBatch.bind(this, id)}><img src={minusIcon} alt="Aufdecken"/></span>
